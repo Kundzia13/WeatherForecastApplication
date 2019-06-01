84eb17eabbcd7a8eb8a0be9967c5e34c40c967a5
Kundzia13 <aleksandra.bobowska.93@gmail.com>
2019-06-01 13:54:17 +0200

    added form

--- src/main/java/com/example/weatherforecast/HomeController.java
+++ src/main/java/com/example/weatherforecast/HomeController.java
@@ -1,15 +1,13 @@
 package com.example.weatherforecast;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String home(ModelMap map){
-        map.put("a",20);
-        return "home";
+    public String getForm(){
+        return "weather";
 
     }
 }
